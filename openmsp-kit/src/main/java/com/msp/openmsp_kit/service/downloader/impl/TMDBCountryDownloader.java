ecf727d147e0169fe69cdc8844bc9feee167a9a6
Tomasz1306 <bogdantomek987@gmail.com>
2025-09-29 22:50:30 +0200

    feat: implemented downloaders

--- openmsp-kit/src/main/java/com/msp/openmsp_kit/service/downloader/impl/TMDBCountryDownloader.java
+++ openmsp-kit/src/main/java/com/msp/openmsp_kit/service/downloader/impl/TMDBCountryDownloader.java
@@ -1,45 +1,45 @@
 package com.msp.openmsp_kit.service.downloader.impl;
 
 import com.msp.openmsp_kit.config.OpenMSPConfig;
-import com.msp.openmsp_kit.model.api.tmdb.TMDBMovieDetailsResponse;
-import com.msp.openmsp_kit.model.result.Result;
-import com.msp.openmsp_kit.model.task.Task;
+import com.msp.openmsp_kit.model.api.tmdb.TMDBCountryResponse;
 import com.msp.openmsp_kit.service.downloader.BuildRequest;
 import com.msp.openmsp_kit.service.downloader.Downloader;
 import com.msp.openmsp_kit.service.parser.JsonParser;
 
 import java.net.URI;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
-public class TMDBMovieDetailsDownloader implements Downloader<TMDBMovieDetailsResponse, String> {
-
+public class TMDBCountryDownloader implements Downloader<List<TMDBCountryResponse>, String> {
     private final OpenMSPConfig config;
     private final JsonParser jsonParser;
     private final HttpClientManager httpClientManager;
 
-    public TMDBMovieDetailsDownloader(HttpClientManager httpClientManager,
-                                      JsonParser jsonParser,
-                                      OpenMSPConfig config) {
+    public TMDBCountryDownloader(HttpClientManager httpClientManager,
+                               JsonParser jsonParser,
+                               OpenMSPConfig config) {
         this.httpClientManager = httpClientManager;
         this.jsonParser = jsonParser;
         this.config = config;
     }
 
     @Override
-    public TMDBMovieDetailsResponse fetch(String movieId) {
+    public List<TMDBCountryResponse> fetch(String movieId) {
         try {
             var jsonBody = httpClientManager
                     .getHttpClient()
                     .send(BuildRequest.buildRequest(buildUri(movieId), config.getTmdbApiKey()),
                             HttpResponse.BodyHandlers.ofString())
                     .body();
-            return jsonParser.parseBody(jsonBody, TMDBMovieDetailsResponse.class);
+            List<TMDBCountryResponse> countries = new ArrayList<>();
+            return jsonParser.parseBody(jsonBody, countries.getClass());
         } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
+            throw new  RuntimeException(e.getMessage());
         }
     }
 
     private URI buildUri(String movieId) {
-        return URI.create(String.format("https://api.themoviedb.org/3/movie/%s?language=pl", movieId));
+        return URI.create(String.format("https://api.themoviedb.org/3/configuration/countries?language=pl"));
     }
 }
--- openmsp-kit/src/main/java/com/msp/openmsp_kit/service/downloader/impl/TMDBGenreDownloader.java
+++ openmsp-kit/src/main/java/com/msp/openmsp_kit/service/downloader/impl/TMDBGenreDownloader.java
@@ -1,23 +1,22 @@
 package com.msp.openmsp_kit.service.downloader.impl;
 
 import com.msp.openmsp_kit.config.OpenMSPConfig;
-import com.msp.openmsp_kit.model.api.tmdb.TMDBMovieDetailsResponse;
-import com.msp.openmsp_kit.model.result.Result;
-import com.msp.openmsp_kit.model.task.Task;
+import com.msp.openmsp_kit.model.api.tmdb.TMDBGenreResponse;
+import com.msp.openmsp_kit.model.api.tmdb.TMDBGenresResponse;
 import com.msp.openmsp_kit.service.downloader.BuildRequest;
 import com.msp.openmsp_kit.service.downloader.Downloader;
 import com.msp.openmsp_kit.service.parser.JsonParser;
 
 import java.net.URI;
 import java.net.http.HttpResponse;
+import java.util.List;
 
-public class TMDBMovieDetailsDownloader implements Downloader<TMDBMovieDetailsResponse, String> {
-
+public class TMDBGenreDownloader implements Downloader<List<TMDBGenreResponse>, String> {
     private final OpenMSPConfig config;
     private final JsonParser jsonParser;
     private final HttpClientManager httpClientManager;
 
-    public TMDBMovieDetailsDownloader(HttpClientManager httpClientManager,
+    public TMDBGenreDownloader(HttpClientManager httpClientManager,
                                       JsonParser jsonParser,
                                       OpenMSPConfig config) {
         this.httpClientManager = httpClientManager;
@@ -26,20 +25,24 @@ public class TMDBMovieDetailsDownloader implements Downloader<TMDBMovieDetailsRe
     }
 
     @Override
-    public TMDBMovieDetailsResponse fetch(String movieId) {
+    public List<TMDBGenreResponse> fetch(String movieId) {
         try {
             var jsonBody = httpClientManager
                     .getHttpClient()
                     .send(BuildRequest.buildRequest(buildUri(movieId), config.getTmdbApiKey()),
                             HttpResponse.BodyHandlers.ofString())
                     .body();
-            return jsonParser.parseBody(jsonBody, TMDBMovieDetailsResponse.class);
+            TMDBGenresResponse response = jsonParser.parseBody(jsonBody, TMDBGenresResponse.class);
+            return response
+                    .genres()
+                    .stream()
+                    .map(genre -> new TMDBGenreResponse(genre.id(), genre.name())).toList();
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
         }
     }
 
     private URI buildUri(String movieId) {
-        return URI.create(String.format("https://api.themoviedb.org/3/movie/%s?language=pl", movieId));
+        return URI.create(String.format("https://api.themoviedb.org/3/genre/movie/list?language=pl"));
     }
 }
