8a7344cad2c6853a1842da409b2db597a2dd1fc7
Tomasz1306 <bogdantomek987@gmail.com>
2025-10-04 19:34:34 +0200

    feat: added rate limiter for files downloading

--- openmsp-kit/src/main/java/com/msp/openmsp_kit/service/file/FileManager.java
+++ openmsp-kit/src/main/java/com/msp/openmsp_kit/service/file/FileManager.java
@@ -4,6 +4,7 @@ import com.msp.openmsp_kit.config.OpenMSPConfig;
 import com.msp.openmsp_kit.model.api.tmdb.TMDBImageResponse;
 import com.msp.openmsp_kit.model.result.Result;
 import com.msp.openmsp_kit.service.downloader.impl.TMDBImagesDownloader;
+import com.msp.openmsp_kit.service.rateLimiter.impl.TMDBFilesRateLimiterImpl;
 import org.springframework.stereotype.Service;
 
 import javax.imageio.ImageIO;
@@ -18,21 +19,29 @@ public class FileManager {
 
     private final OpenMSPConfig openMSPConfig;
     private final TMDBImagesDownloader tmdbImagesDownloader;
+    private final TMDBFilesRateLimiterImpl tmdbFilesRateLimiterImpl;
 
     public FileManager(OpenMSPConfig openMSPConfig,
-                       TMDBImagesDownloader tmdbImagesDownloader) {
+                       TMDBImagesDownloader tmdbImagesDownloader,
+                       TMDBFilesRateLimiterImpl tmdbFilesRateLimiterImpl) {
         this.openMSPConfig = openMSPConfig;
         this.tmdbImagesDownloader = tmdbImagesDownloader;
+        this.tmdbFilesRateLimiterImpl = tmdbFilesRateLimiterImpl;
     }
 
     public void downloadFile(Result<?> result) {
+        tmdbFilesRateLimiterImpl.acquire();
         BufferedImage image = tmdbImagesDownloader.fetch(result.taskId());
+        if (image == null) {
+            System.err.println("Buffered Image is null");
+            return;
+        }
         saveFile(image, (TMDBImageResponse) result.data());
     }
 
     private void saveFile(BufferedImage bufferedImage, TMDBImageResponse imageDomain) {
         try {
-            String imageDirectoryPath = openMSPConfig.getImagesDestPath() + "/" + imageDomain.getTmdbId() + "/" + imageDomain.getIso_639_1();
+            String imageDirectoryPath = openMSPConfig.getImagesDestPath() + "/" + imageDomain.getTmdbId() + "/" + imageDomain.getType() + "/" + imageDomain.getIso_639_1();
             Files.createDirectories(Paths.get(imageDirectoryPath));
             ImageIO.write(bufferedImage,
                     "png",
