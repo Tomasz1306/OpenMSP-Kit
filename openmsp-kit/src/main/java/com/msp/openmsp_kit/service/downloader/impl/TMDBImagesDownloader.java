c403fcc7c7cddc776cfe1b9676b0a5ae884583f7
Tomasz1306 <bogdantomek987@gmail.com>
2025-10-04 19:29:43 +0200

    feat: changed to downloading image with httpClient for performance, changed size of downloading images

--- openmsp-kit/src/main/java/com/msp/openmsp_kit/service/downloader/impl/TMDBImagesDownloader.java
+++ openmsp-kit/src/main/java/com/msp/openmsp_kit/service/downloader/impl/TMDBImagesDownloader.java
@@ -1,29 +1,45 @@
 package com.msp.openmsp_kit.service.downloader.impl;
 
-import com.msp.openmsp_kit.config.OpenMSPConfig;
+import com.msp.openmsp_kit.service.downloader.BuildRequest;
 import com.msp.openmsp_kit.service.downloader.Downloader;
-import com.msp.openmsp_kit.service.parser.JsonParser;
 import org.springframework.stereotype.Service;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
-import java.net.URL;
+import java.net.http.HttpResponse;
 
 @Service
 public class TMDBImagesDownloader implements Downloader<BufferedImage, String> {
 
+    private final HttpClientManager httpClientManager;
+
+    TMDBImagesDownloader(HttpClientManager httpClientManager) {
+        this.httpClientManager = httpClientManager;
+    }
+
     @Override
     public BufferedImage fetch(String imagePath) {
         try {
-            return ImageIO.read(new URL(buildUri(imagePath).toString()));
+            byte[] response = httpClientManager
+                    .getHttpClient()
+                    .send(BuildRequest.buildRequest(buildUri(imagePath)), HttpResponse.BodyHandlers.ofByteArray())
+                    .body();
+            InputStream is = new ByteArrayInputStream(response);
+            BufferedImage image = ImageIO.read(is);
+            is.close();
+            return image;
         } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
 
     private URI buildUri(String imagePath) {
-        return URI.create(String.format("https://image.tmdb.org/t/p/original%s", imagePath));
+        return URI.create(String.format("https://image.tmdb.org/t/p/w500%s", imagePath));
     }
 }
