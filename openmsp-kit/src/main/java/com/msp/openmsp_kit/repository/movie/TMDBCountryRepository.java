f081eb05df032cce87c0a94f649f5c4c652a6ef4
Tomasz1306 <bogdantomek987@gmail.com>
2025-10-05 20:56:27 +0200

    feat: added queries

--- openmsp-kit/src/main/java/com/msp/openmsp_kit/repository/movie/TMDBCountryRepository.java
+++ openmsp-kit/src/main/java/com/msp/openmsp_kit/repository/movie/TMDBCountryRepository.java
@@ -1,10 +1,12 @@
 package com.msp.openmsp_kit.repository.movie;
 
-import com.msp.openmsp_kit.model.persistence.entity.TMDBMovieEntity;
 import com.msp.openmsp_kit.model.persistence.entity.TMDBProductionCountryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TMDBCountryRepository extends JpaRepository<TMDBProductionCountryEntity, Long> {
+    Optional<TMDBProductionCountryEntity> findByIso31661(String iso_639_1);
 }
--- openmsp-kit/src/main/java/com/msp/openmsp_kit/repository/movie/TMDBLanguageRepository.java
+++ openmsp-kit/src/main/java/com/msp/openmsp_kit/repository/movie/TMDBLanguageRepository.java
@@ -1,10 +1,12 @@
 package com.msp.openmsp_kit.repository.movie;
 
-import com.msp.openmsp_kit.model.persistence.entity.TMDBMovieEntity;
 import com.msp.openmsp_kit.model.persistence.entity.TMDBSpokenLanguageEntity;
 import jakarta.data.repository.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 @Repository
 public interface TMDBLanguageRepository extends JpaRepository<TMDBSpokenLanguageEntity, Long> {
+    Optional<TMDBSpokenLanguageEntity> findByIso6391(String iso_639_1);
 }
