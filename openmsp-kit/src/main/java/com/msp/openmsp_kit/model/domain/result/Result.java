334bd983926503039e3a2452a033c37b14a08d85
Tomasz1306 <bogdantomek987@gmail.com>
2025-10-06 21:01:09 +0200

    feat: added priority to result

--- openmsp-kit/src/main/java/com/msp/openmsp_kit/model/domain/result/Result.java
+++ openmsp-kit/src/main/java/com/msp/openmsp_kit/model/domain/result/Result.java
@@ -1,8 +1,11 @@
 package com.msp.openmsp_kit.model.domain.result;
 
+import com.msp.openmsp_kit.model.domain.common.Priority;
+
 public record Result<T> (
         String taskId,
         boolean success,
         T data,
-        String error
+        String error,
+        Priority priority
 ) {}
