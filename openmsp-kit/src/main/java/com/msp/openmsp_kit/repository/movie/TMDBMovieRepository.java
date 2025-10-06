2e1a131dbce18f50e5619ed5d70f322b29b384d9
Tomasz1306 <bogdantomek987@gmail.com>
2025-10-06 21:00:48 +0200

    feat: added queries

--- openmsp-kit/src/main/java/com/msp/openmsp_kit/repository/movie/TMDBMovieRepository.java
+++ openmsp-kit/src/main/java/com/msp/openmsp_kit/repository/movie/TMDBMovieRepository.java
@@ -4,7 +4,11 @@ import com.msp.openmsp_kit.model.persistence.entity.TMDBMovieEntity;
 import jakarta.data.repository.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 @Repository
 public interface TMDBMovieRepository extends JpaRepository<TMDBMovieEntity, Long> {
 
+    List<TMDBMovieEntity> findAllByTmdbId(Integer movieId);
+    TMDBMovieEntity findByTmdbIdAndIso6391(Integer movieId, String iso_639_1);
 }
